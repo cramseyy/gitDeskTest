6d666434a668c8d48559d42b5b55ecc51d3c00a0
cramseyy <164997522+cramseyy@users.noreply.github.com>
2024-05-14 23:45:06 -0400

    added scanner

--- test.java
+++ test.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter some text: ");
+        String text = scan.nextLine();
+
         System.out.println("Hello World");
+        System.out.println(text);
     }
 }
