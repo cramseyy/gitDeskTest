defba4282e9296385fe758b9371769e9fb3efaea
cramseyy <164997522+cramseyy@users.noreply.github.com>
2024-05-14 23:52:12 -0400

    added add function

--- test.java
+++ test.java
@@ -4,10 +4,26 @@ public class test {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter some text: ");
+        System.out.print("Enter some text: ");
         String text = scan.nextLine();
-
-        System.out.println("Hello World");
         System.out.println(text);
+
+        System.out.print("Enter a number: ");
+        Scanner num1 = new Scanner(System.in);
+        int number1 = num1.nextInt();
+
+        System.out.print("Enter another number: ");
+        Scanner num2 = new Scanner(System.in);
+        int number2 = num2.nextInt();
+
+        System.out.println("Sum" + add(number1,number2));
+
+        scan.close();
+        num1.close();
+        num2.close();
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
